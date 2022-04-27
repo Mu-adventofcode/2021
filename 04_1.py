3d2f574a409a9bf8fb2fc5d7c3353591e541406a
Mu <mu@nomail>
2022-04-27 19:07:54 +0200

    day 4

---
+++ 04_1.py
@@ -0,0 +1,43 @@
+"""
+Play bingo until one board wins.  Display the final score of the winning board.
+"""
+import numpy as np
+
+
+class Board:
+    """
+    A matrix of 5x5 items.
+    """
+
+    def __init__(self, lines):
+        self.items = np.array([line.split() for line in lines], dtype=int)
+        self.marked = np.zeros((5, 5), dtype=np.bool)
+
+    def mark(self, draw):
+        self.marked[self.items == int(draw)] = True
+
+    def won(self):
+        winning_row = np.any(np.all(self.marked, axis=0))
+        winning_col = np.any(np.all(self.marked, axis=1))
+        return winning_row or winning_col
+
+    def score(self, lastdraw):
+        return np.sum(self.items[self.marked == False]) * int(lastdraw)
+
+
+def main(filename):
+    # parse input file
+    with open(filename) as txt:
+        lines = txt.readlines()
+    draws = lines[0].split(",")
+    boards = [Board(lines[idx : idx + 5]) for idx in range(2, len(lines), 6)]
+    # play
+    for draw in draws:
+        for board in boards:
+            board.mark(draw)
+            if board.won():
+                return board.score(draw)
+
+
+if __name__ == "__main__":
+    print(main("input.txt"))
