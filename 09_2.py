567ee2f5950d11b7553f8321381dc4e429090fe0
Mu <mu@nomail>
2022-04-29 08:43:37 +0200

    09_2: introduce wall points to simplify code.
    Introducing 'walls': points of height 9, both within the data matrix and
    (extra) points surrounding it. The concept of wall points makes the code
    more readable. A lot of commenting becomes obsolete and is also removed.

--- 09_2.py
+++ 09_2.py
@@ -1,49 +1,32 @@
 """
 This solution makes use of networkx's ability to split up graphs into connected
-components. The script scans through the matrix of heights. It locally connects
-points that will be the same basin. Eventually the networkx components will
-correspond to basins.
+components. The script scans all matrix points. It locally connects points that
+are lower than 9. The distinct networkx components will correspond to basins.
 """
 import networkx as nx
-from functools import reduce
-
 
 with open("input.txt") as f:
-    data = [[int(d) for d in row.strip()] for row in f]
-max_x = len(data[0]) - 1
-max_y = len(data) - 1
-connections = set()
+    matrix = [[int(d) for d in row.strip()] for row in f]
 
-# Scan all points in the data and create connections.
-for y, row in enumerate(data):
-    for x, height in enumerate(row):
-        if height == 9:
+walls = [[1 if d == 9 else 0 for d in row] for row in matrix]
+walls = [[1] + row + [1] for row in walls]
+dx = len(walls[0])
+walls = [[1] * dx] + walls + [[1] * dx]
+dy = len(walls)
+
+connections = set()
+for y in range(1, dy - 1):
+    for x in range(1, dx - 1):
+        if walls[y][x]:
             continue
-        # Create a small set: 1 to 5 neighboring points around (y, x). Because
-        # points at height 9 are excluded, this will be a subset of a basin.
-        subset = set(
-            point
-            for point in [
-                (y, x),
-                (y, x - 1) if x > 0 else (y, x),
-                (y, x + 1) if x < max_x else (y, x),
-                (y - 1, x) if y > 0 else (y, x),
-                (y + 1, x) if y < max_y else (y, x),
-            ]
-            if data[point[0]][point[1]] != 9
-        )
-        # Connect the points of the subset.  As a point will be in other
-        # subsets too, eventually all points within one basin will become
-        # (directly or indirectly) connected.
-        llg = list(subset)
+        cross = set(((y, x), (y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)))
+        notwall = set((v, u) for (v, u) in cross if not walls[v][u])
+        llg = list(notwall)
         for conn in zip(llg, llg[1:]):
             connections.add(conn)
 
-# Create a graph and split it up into its distinct components (=basins).
 G = nx.Graph()
 G.add_edges_from(connections)
 basins = nx.connected_components(G)
-basin_sizes = [len(b) for b in basins]
-basin_sizes.sort(reverse=True)
-result = reduce(lambda x, y: x * y, basin_sizes[:3])
-print(result)
+basin_sizes = sorted([len(b) for b in basins], reverse=True)
+print(basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
