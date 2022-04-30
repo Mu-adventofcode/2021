b605ff9c91e3aa47dcbf777b016369e751f275f9
Mu <mu@nomail>
2022-04-30 22:16:29 +0200

    Moved scripts to gists.

--- 08_1.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-with open("input.txt") as txt:
-    output_values = [line.split(" | ")[1].strip() for line in txt]
-digit_lengths = [len(d) for val in output_values for d in val.split()]
-counts = Counter(digit_lengths)
-total = sum(counts[i] for i in (2, 4, 3, 7))  # lens of digits 1, 4, 7 resp 8
-print(total)
--- template.py
+++
@@ -1,8 +0,0 @@
-def main():
-    with open("input.txt") as f:
-        data = f.readlines()
-    print(len(data))
-
-
-if __name__ == "__main__":
-    main()
